0b4a7194c64803ec61f73f9be63df208f57a444f
Cauan Ricardo <cauanrricardo@gmail.com>
2024-09-04 14:18:26 -0300

    Adicao da pasta de ponteiros

---
+++ Ponteiros/01.Declaração e inicialização de ponteiros.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    //declaracao de variaveis + ponteiro
+    int num = 10;
+    int *int_ptr = &num;
+    
+    printf("%d %d",num, *int_ptr);
+    
+    
+}
